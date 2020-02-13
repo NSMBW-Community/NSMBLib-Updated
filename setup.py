d11c68df96ea869186e00025f96c91677e324027
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2020-02-13 02:06:55 -0700

    Fix version num, and switch from distutils to setuptools
    This allows us to build with bdist_wheel, which is great

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 from distutils.extension import Extension
 
 setup(
   name='nsmblib',
-  version='0.5',
+  version='0.4.1',
   ext_modules=[
     Extension(
       'nsmblib',
