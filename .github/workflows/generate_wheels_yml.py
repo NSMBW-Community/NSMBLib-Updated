6684bfc0bf0a756b785b88524c42a5fcd0fbb89e
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2020-02-13 19:45:07 -0700

    I forgot to have it call auditwheel
    aaaaaaaaaaaaaaaaaaaa

--- .github/workflows/generate_wheels_yml.py
+++ .github/workflows/generate_wheels_yml.py
@@ -44,42 +44,63 @@ yml.append("""
         path: dist
 """)
 
-# Linux builds: we need one job for each Python version because GitHub actions is stupid
-for pyver in ['27', '35', '36', '37', '38']:
-    # In particular, there seems to be no way (as of 2020-02-13) to accomplish this in the YAML
+# Python 2.7
+yml.append("""
+  linux-27:
+
+    strategy:
+      matrix:
+        python-version: [2.7]
+
+    runs-on: ubuntu-latest
+
+    steps:
+    - uses: actions/checkout@v2
+    - name: Install dependencies
+      run: |
+        python -m pip install --upgrade pip
+        python -m pip install setuptools wheel
+    - name: Build
+      run: |
+        python setup.py bdist_wheel
+    - name: Upload artifacts
+      uses: actions/upload-artifact@v1
+      with:
+        name: build-manylinux-{pyver}
+        path: dist
+""")
+
+# manylinuxes for 3.x
+for pyver in ['35', '36', '37', '38']:
+    # There seems to be no way (as of 2020-02-13) to accomplish this in the YAML
     # (unlike in Azure Pipelines)
-    if pyver == '27':
-        pycommand = 'python'
-    elif pyver == '38':
+    if pyver == '38':
         pycommand = '/opt/python/cp38-cp38/bin/python'
     else:
         pycommand = f'/opt/python/cp{pyver}-cp{pyver}m/bin/python'
 
     yml.append(f"""
   manylinux-{pyver}:
-""")
 
-    # We don't build manylinux wheels for 2.7; just use the provided one
-    if pyver == '27':
-        yml.append(f"""
-    strategy:
-      matrix:
-        python-version: [2.7]
-""")
-
-    yml.append(f"""
     runs-on: ubuntu-latest
-    {'container: quay.io/pypa/manylinux2014_x86_64' if pyver != '27' else ''}
+    container: quay.io/pypa/manylinux2014_x86_64
 
     steps:
     - uses: actions/checkout@v2
     - name: Install dependencies
       run: |
         {pycommand} -m pip install --upgrade pip
-        {pycommand} -m pip install setuptools wheel
+        {pycommand} -m pip install setuptools wheel auditwheel
     - name: Build
       run: |
         {pycommand} setup.py bdist_wheel
+    - name: Run auditwheel
+      run: |
+        mkdir dist/wheelhouse
+        {pycommand} -m auditwheel repair -w dist/wheelhouse/ bindings/dist/*.whl
+        rm dist/*.whl
+        mv dist/wheelhouse/* bindings/dist/
+        rm -rf dist/wheelhouse
     - name: Upload artifacts
       uses: actions/upload-artifact@v1
       with:
