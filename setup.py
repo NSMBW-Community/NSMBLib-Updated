2f539b3b7962d93d31eb166cd479b27f7c8b4e68
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2020-11-13 12:54:04 -0500

    Changes

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     Extension(
       'nsmblib',
       sources=['nsmblibmodule.c', 'list.c'],
-      extra_compile_args=['-std=c95'],
+      extra_compile_args=['-Wno-implicit-function-declaration'],
       py_limited_api=True,
     )
   ]
