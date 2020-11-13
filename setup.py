fcb91a111a590f09c5f3f249dc54b11cffd4ddf4
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2020-11-13 13:11:00 -0500

    Changes

--- setup.py
+++ setup.py
@@ -12,14 +12,21 @@ if sys.version_info[0] > 2 and os.name != 'nt':
     if any('bdist' in arg for arg in sys.argv[1:]):
         sys.argv.extend(['--py-limited-api', pyver])
 
+# Workaround for what looks like a weird Python bug?
+if os.name == 'nt':
+    extra_compile_args = []
+else:
+    extra_compile_args = ['-Wno-implicit-function-declaration']
+
 setup(
   name='nsmblib',
   version='2020.10.07.0',
+  py_modules=['lz77'],
   ext_modules=[
     Extension(
       'nsmblib',
       sources=['nsmblibmodule.c', 'list.c'],
-      extra_compile_args=(['-Wno-implicit-function-declaration'] if (os.name != 'nt') else []),
+      extra_compile_args=extra_compile_args,
       py_limited_api=True,
     )
   ]
